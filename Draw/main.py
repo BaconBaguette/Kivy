abee3694eaa8a6482c697ff5c3416cf7f64ef88e
Jack Turner <the.jack.turner@gmail.com>
2020-03-16 16:29:32 +0000

    Just keeping stuff updated

--- Draw/main.py
+++ Draw/main.py
@@ -3,16 +3,22 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.slider import Slider
 
 from kivy.uix.widget import Widget
-from kivy.graphics import Rectangle, Color
+from kivy.graphics import Rectangle, Color, Triangle
 
 class DrawingWidget(Widget):
     def __init__(self):
         super(DrawingWidget, self).__init__()
 
         with self.canvas:
-            Color(1, 0, 0, 1)
-            Rectangle(size=(300, 100),
-                      pos=(300, 200))
+            Color(1, 1, 1, 1)
+            self.rect = Rectangle(size=self.size,
+                                  pos=self.pos)
+        self.bind(pos=self.update_rectangle,
+                  size=self.update_rectangle)
+
+    def update_rectangle(self, instance, value):
+        self.rect.pos = self.pos
+        self.rect.size = self.size
 
 class DrawingApp(App):
     def build(self):
